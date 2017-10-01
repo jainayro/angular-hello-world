1357573e57aa687c3d4a1c86092f45676225e006
unknown <jainayrogeorge@outlook.com>
2017-10-01 09:52:22 -0400

    done creating todo list, created user item component

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-user-item',
+  selector: 'user-item',
   templateUrl: './user-item.component.html',
-  styleUrls: ['./user-item.component.scss']
+  styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
+  @Input() name: string;
 
-  name : string;
-
-  constructor() {
-    this.name = 'Jota';
-
-   }
+  constructor() { }
 
   ngOnInit() {
   }
